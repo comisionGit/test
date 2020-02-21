f14c9513a4ffb6d49cdfd09716ced13c31965b0f
yusi <yusi118@gmail.com>
2020-02-21 12:39:56 +0100

    metodo arreglado roberto

--- prueba1/src/main/java/model/comi.java
+++ prueba1/src/main/java/model/comi.java
@@ -17,16 +17,10 @@ public class comi {
   }
   
   public static  void metodoRoberto() {
-<<<<<<< HEAD
+
 	  System.out.println("hola roberto.2");
 	  System.out.println("actualizado desde la rama.");
-=======
-	  System.out.println("hola roberto.2");
 
-	  System.out.println("actualizado desde la rama.");
-
->>>>>>> branch 'development' of https://github.com/comisionGit/test.git
- 
   }
   public void imprimir(){
 	  
